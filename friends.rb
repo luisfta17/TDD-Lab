b84772657c389de061722028b3aebfe40a77de1b
Luis Tejero <luisfaridta@gmail.com>
2018-04-12 14:42:46 +0100

    exercises 1 2 3 4 done

---
+++ friends.rb
@@ -0,0 +1,21 @@
+def get_name(person)
+  return person[:name]
+end
+
+def get_show(person)
+  return person[:favourites][:tv_show]
+end
+
+def like_food(person, fav_food)
+  for food in person[:favourites][:things_to_eat]
+    if (food == fav_food)
+      return true
+    end
+  end
+  return false
+end
+
+def add_friend(person, friend_name)
+  person[:friends].push(friend_name)
+  return person[:friends].length
+end
